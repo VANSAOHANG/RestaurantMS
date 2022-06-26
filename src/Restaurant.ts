311caf1cf28b7063453b018d7e42668263d42ca9
Vansao.HANG <vansao.hang@pnc.lan>
2022-06-26 18:57:43 +0700

    pull code

--- src/Restaurant.ts
+++ src/Restaurant.ts
@@ -1,5 +1,8 @@
 import { Address } from "./Address";
+import { HumanManager } from "./HumanManager/HumanManagement";
 
 export class Restaurant {
     constructor(private name: string, private address: Address) { }
+  public hr: HumanManager = new HumanManager();
+
 }
